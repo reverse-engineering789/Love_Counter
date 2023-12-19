89b6f6052e22cb97c968c40e8bb2a6ad57beaaa0
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2023-12-19 16:11:56 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+
+  const [count, setCount] = useState(0);
+
+
+  function Increment(){
+    setCount(count + 1);
+  }
+
+  function Decrement(){
+    setCount(count - 1);
+  }
+
+  function reset(){
+    setCount(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='Heading'>Increment && Decrement</div>
+
+      <div className='SetOne'>
+      <div className='IncrementHandler'> 
+      <button className='AddButton' type='submit' onClick={Increment}> + </button> 
+      </div>
+      <div className='Display'>{count}</div>
+      {/* <div className='DecrementHandler'> - </div> */}
+      <div className='DecrementHandler'> 
+      <button  className='SubtractButton' type='submit' onClick={Decrement} > - </button> 
+      </div>
+      </div>
+
+      <div className='RestartHandler'>
+        <button className='RestartButton' type='submit' onClick={reset}>Restart</button>
+      </div>
     </div>
   );
 }
